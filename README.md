32507fdcd1c6d693bfc3162841644089c65b146c
cheretbe <a.a.orlov@gmail.com>
2021-04-27 16:16:30 +0200

    Change to Ubuntu 20.04 and latest Ansible (not finished)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # packer-container-tests
-Ubuntu 18.04 with Docker CE, LXD and testing tools installed
+Ubuntu 20.04 with Docker CE, LXD and testing tools installed
 
 ```shell
 packer build -force -on-error=ask packer.json
--- provision/install_ansible.sh
+++ provision/install_ansible.sh
@@ -4,11 +4,24 @@ set -euo pipefail
 
 if [ "$UID" != "0" ]; then
   echo "Re-running the script with sudo"
-  sudo $0 $*
+  echo sudo su - root $0 $*
+  sudo su - root $0 $*
   exit
 fi
 
 echo "Installing Ansible"
-add-apt-repository ppa:ansible/ansible -y
+# add-apt-repository ppa:ansible/ansible -y
+echo "Updating package cache"
 apt-get update -y -qq
-DEBIAN_FRONTEND=noninteractive apt-get install -y -qq ansible
+echo "Installing python3-pip package"
+DEBIAN_FRONTEND=noninteractive apt-get install -y -qq python3-pip
+
+echo "Upgrading pip3"
+# Temporary solution to suppress messages like this:
+# WARNING: Value for scheme.scripts does not match ...
+# https://github.com/pypa/pip/issues/9617
+# https://stackoverflow.com/questions/67244301/warning-messages-when-i-update-pip-or-install-packages/67250419#67250419
+python3 -m pip install pip==21.0.1
+
+echo "Installing Ansible using pip3"
+pip3 --disable-pip-version-check install ansible
